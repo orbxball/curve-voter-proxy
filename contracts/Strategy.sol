99053bd685abd2963d63637d4e576d553d3b655c
orbxball <orbxball@protonmail.com>
2021-04-18 10:30:24 -0400

    add two base stablecoins, usdc & usdt

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -132,6 +132,8 @@ abstract contract CurveVoterProxy is BaseStrategy {
 
     address public constant crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
     address public constant dai = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    address public constant usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address public constant usdt = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     address public constant weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     address public constant wbtc = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
 
