39fd8f497541f0afd24749bcb7b9bc897a13fa39
orbxball <orbxball@protonmail.com>
2021-04-25 00:28:06 -0400

    complete IVoterProxy interface

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -108,14 +108,15 @@ interface ICurveFi {
 }
 
 interface IVoterProxy {
+    function proxy() external returns (address);
+    function balanceOf(address _gauge) external view returns (uint256);
+    function deposit(address _gauge, address _token) external;
     function withdraw(
         address _gauge,
         address _token,
         uint256 _amount
     ) external returns (uint256);
-    function balanceOf(address _gauge) external view returns (uint256);
     function withdrawAll(address _gauge, address _token) external returns (uint256);
-    function deposit(address _gauge, address _token) external;
     function harvest(address _gauge) external;
     function lock() external;
     function approveStrategy(address) external;
