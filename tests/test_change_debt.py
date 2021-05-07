20c164ded12987b77e50aaed318231cd4efacab5
orbxball <orbxball@protonmail.com>
2021-05-06 21:54:47 -0400

    upgrade test on changing debt ratio

--- tests/test_change_debt.py
+++ tests/test_change_debt.py
@@ -5,8 +5,19 @@ def test_change_debt(gov, token, vault, strategy, whale, amount):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": whale})
     vault.deposit(amount, {"from": whale})
+    strategy.harvest()
+    print(f"debt ratio: {vault.strategies(strategy).dict()['debtRatio']}")
+    print(f"strategy assets: {strategy.estimatedTotalAssets()}")
+    print(f"vault total assets: {vault.totalAssets()}")
+
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.harvest()
+    print(f"debt ratio: {vault.strategies(strategy).dict()['debtRatio']}")
+    print(f"strategy assets: {strategy.estimatedTotalAssets()}")
+    print(f"vault total assets: {vault.totalAssets()}")
 
     vault.updateStrategyDebtRatio(strategy.address, 10_000, {"from": gov})
-    strategy.tend()
+    strategy.harvest()
+    print(f"debt ratio: {vault.strategies(strategy).dict()['debtRatio']}")
+    print(f"strategy assets: {strategy.estimatedTotalAssets()}")
+    print(f"vault total assets: {vault.totalAssets()}")
