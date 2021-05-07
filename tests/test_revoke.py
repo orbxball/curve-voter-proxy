31b109e747ad2b6bd2d7dc473f0dc07c4a341476
orbxball <orbxball@protonmail.com>
2021-05-06 21:59:05 -0400

    upgrade test on revoke strategy

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -1,7 +1,14 @@
 import pytest
 
 
-def test_revoke(gov, vault, strategy, token):
+def test_revoke(gov, vault, strategy, token, whale, amount):
+    scale = 10 ** token.decimals()
+    # Deposit to the vault
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(amount, {"from": whale})
+    assert token.balanceOf(vault.address) == amount
+    strategy.harvest({"from": gov})
+
     vault.revokeStrategy(strategy, {"from": gov})
     strategy.harvest({"from": gov})
 
