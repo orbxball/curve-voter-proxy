239a22f725cf31128584c794eecaa1f482cc2c03
orbxball <orbxball@protonmail.com>
2021-05-06 22:03:00 -0400

    upgrade test on emergency exit

--- tests/test_emergency.py
+++ tests/test_emergency.py
@@ -1,7 +1,14 @@
 import pytest
 
 
-def test_emergency_exit(gov, vault, strategy, token):
+def test_emergency_exit(gov, vault, strategy, token, whale, amount):
+    scale = 10 ** token.decimals()
+    # Deposit to the vault
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(amount, {"from": whale})
+    assert token.balanceOf(vault.address) == amount
+    strategy.harvest({"from": gov})
+
     strategy.setEmergencyExit({"from": gov})
     strategy.harvest({"from": gov})
 
